167ee33bc44af90d6f7e91d8bc93892f7b2b8a2a
DesktopECHO <33142753+DesktopECHO@users.noreply.github.com>
2023-09-29 11:12:19 -0300

    Revival work continues...
    (╯°□°)╯︵ ┻━┻

--- dist/etc/xrdp/startwm.sh
+++ dist/etc/xrdp/startwm.sh
@@ -1,30 +1,47 @@
 #!/bin/sh
-# xrdp X session start script (c) 2015, 2017 mirabilos
+# xrdp X session start script (c) 2015, 2017, 2021 mirabilos
 # published under The MirOS Licence
-# export KDE_DEBUG=0
+
+# Rely on /etc/pam.d/xrdp-sesman using pam_env to load both
+# /etc/environment and /etc/default/locale to initialise the
+# locale and the user environment properly.
 
 if test -r /etc/profile; then
         . /etc/profile
 fi
 
-if test -r /etc/default/locale; then
-        . /etc/default/locale
-        test -z "${LANG+x}" || export LANG
-        test -z "${LANGUAGE+x}" || export LANGUAGE
-        test -z "${LC_ADDRESS+x}" || export LC_ADDRESS
-        test -z "${LC_ALL+x}" || export LC_ALL
-        test -z "${LC_COLLATE+x}" || export LC_COLLATE
-        test -z "${LC_CTYPE+x}" || export LC_CTYPE
-        test -z "${LC_IDENTIFICATION+x}" || export LC_IDENTIFICATION
-        test -z "${LC_MEASUREMENT+x}" || export LC_MEASUREMENT
-        test -z "${LC_MESSAGES+x}" || export LC_MESSAGES
-        test -z "${LC_MONETARY+x}" || export LC_MONETARY
-        test -z "${LC_NAME+x}" || export LC_NAME
-        test -z "${LC_NUMERIC+x}" || export LC_NUMERIC
-        test -z "${LC_PAPER+x}" || export LC_PAPER
-        test -z "${LC_TELEPHONE+x}" || export LC_TELEPHONE
-        test -z "${LC_TIME+x}" || export LC_TIME
-        test -z "${LOCPATH+x}" || export LOCPATH
+if test -r $HOME/.profile; then
+        . $HOME/.profile
 fi
 
-dbus-run-session startplasma-x11
+STARTUP=default
+
+if [ -n "$DESKTOP_SESSION" ] && [ -d /usr/share/xsessions ] \
+  && [ -f "/usr/share/xsessions/$DESKTOP_SESSION.desktop" ]; then
+  STARTUP=$(grep ^Exec= "/usr/share/xsessions/$DESKTOP_SESSION.desktop")
+  STARTUP=${STARTUP#Exec=*}
+  XDG_CURRENT_DESKTOP=$(grep ^DesktopNames= "/usr/share/xsessions/$DESKTOP_SESSION.desktop")
+  XDG_CURRENT_DESKTOP=${XDG_CURRENT_DESKTOP#DesktopNames=*}
+  export XDG_CURRENT_DESKTOP
+fi
+
+if [ "$XRDP_USE_MULTISESSION" = 1 ]; then
+  eval $( /etc/xrdp/systemd_user_context.sh init -p $$ )
+fi
+
+test -d "$XDG_RUNTIME_DIR" && echo $XDG_SESSION_ID > $XDG_RUNTIME_DIR/login-session-id
+
+if [ "$STARTUP" = "default" ]; then
+  if test -x /etc/X11/Xsession; then
+      exec /etc/X11/Xsession
+  else
+      exec /bin/sh /etc/X11/Xsession
+  fi
+else
+  if test -x /etc/X11/Xsession; then
+      exec /etc/X11/Xsession "$STARTUP"
+  else
+      exec /bin/sh /etc/X11/Xsession "$STARTUP"
+  fi
+fi
+
